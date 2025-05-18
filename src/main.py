859bd397307607cfbe650a3f5e614d48cef3fbdb
Shun Kinoshita <shunthink@outlook.jp>
2025-05-18 23:54:52 +0900

    Add main.py for Real GDP(value) plot (Ver1.0)

--- src/main.py
+++ src/main.py
@@ -1,15 +1,81 @@
 # src/main.py
 
+import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-# Imaginary data
-years = [2018, 2019, 2020, 2021, 2022]
-gdp = [500, 520, 480, 530, 550]
 
-plt.plot(years, gdp, marker='o')
-plt.title("Japan GDP")
+# 実質GDP(value)
+## CSVファイルのパス
+csv_path = os.path.join("data", "processed", "gdp", "realGDP_value_total.csv")
+
+## CSVの読み込み
+df = pd.read_csv(csv_path)
+
+## グラフ描画
+plt.figure(figsize=(8, 5))
+plt.plot(df["Year"], df["GDP"], marker='o', linestyle='-')
+
+plt.title("Real GDP of Japan")
 plt.xlabel("Year")
-plt.ylabel("GDP (trilion yen)")
+plt.ylabel("GDP (Billion yen)")
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+
+# # 実質GDPのうち、家計(value)
+# ## CSVファイルのパス
+# csv_path = os.path.join("data", "processed", "gdp", "realGDP_value_private.csv")
+
+# ## CSVの読み込み
+# df = pd.read_csv(csv_path)
+
+# ## グラフ描画
+# plt.figure(figsize=(8, 5))
+# plt.plot(df["Year"], df["Private"], marker='o', linestyle='-')
+
+# plt.title("Real GDP of Japan")
+# plt.xlabel("Year")
+# plt.ylabel("Household consumption as a factor of real GDP (Billion yen)")
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+
+
+# # 実質GDPのうち、政府(value)
+# ## CSVファイルのパス
+# csv_path = os.path.join("data", "processed", "gdp", "realGDP_value_government.csv")
+
+# ## CSVの読み込み
+# df = pd.read_csv(csv_path)
+
+# ## グラフ描画
+# plt.figure(figsize=(8, 5))
+# plt.plot(df["Year"], df["Government"], marker='o', linestyle='-')
+
+# plt.title("Real GDP of Japan")
+# plt.xlabel("Year")
+# plt.ylabel("Government consumption as a factor of real GDP (Billion yen)")
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+
+
+# # 実質GDPのうち、純輸出(value)
+# ## CSVファイルのパス
+# csv_path = os.path.join("data", "processed", "gdp", "realGDP_value_exports.csv")
+
+# ## CSVの読み込み
+# df = pd.read_csv(csv_path)
+
+# ## グラフ描画
+# plt.figure(figsize=(8, 5))
+# plt.plot(df["Year"], df["NetExports"], marker='o', linestyle='-')
+
+# plt.title("Real GDP of Japan")
+# plt.xlabel("Year")
+# plt.ylabel("Net Exports as a factor of real GDP (Billion yen)")
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
